57fe5dbef3f6866ea2b62b4558fa4642c2ac3c77
“Rahul <dhakarahul16@gmail.com>
2024-08-30 14:24:03 +0530

    --

--- StringExample.java
+++ StringExample.java
@@ -1,14 +1,16 @@
-
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class StringExample {
     public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
-    
+        int[] arr = {1,2,3,45,6};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
     }
-    
-    static String greet(){
-        return "Hello World!";
+
+    static void change(int[] arr) {
+        arr[0] = 99;
     }
     
 }
